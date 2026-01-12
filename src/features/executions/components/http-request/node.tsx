753fce1338436c5e5361b7224dc90a8456a0903c
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-13 03:16:25 +0530

    feat: Implement HttpRequestNode and associated dialog for configuring HTTP requests

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -2,8 +2,10 @@
 
 import type { Node, NodeProps } from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
+import { HttpRequestDialog } from "./dialog";
+
 
 type HttpRequestNodeData = {
   endPoint?: string;
@@ -15,15 +17,27 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const nodeStatus = "initial";
+
+  const handleOpenSettings = () => setDialogOpen(true);
+
   const nodeData = props.data;
   const description = nodeData?.endPoint
     ? `${nodeData.method || "GET"} ${nodeData.endPoint}`
     : "Not configured";
 
-    const nodeStatus = "error";
-
   return (
     <>
+      <HttpRequestDialog 
+        open={dialogOpen} 
+        onOpenChange={setDialogOpen} 
+        onSubmit={() => {}}
+        defaultEndpoint={nodeData.endPoint} // TODO: Check if it can be improved by just sending initialValues={nodeData}
+        defaultMethod={nodeData.method} 
+        defaultBody={nodeData.body}  
+      />
       <BaseExecutionNode
         {...props}
         id={props.id}
@@ -31,8 +45,8 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         name="HTTP Request"
         description={description}
         status={nodeStatus}
-        onSettings={() => {}}
-        onDoubleClick={() => {}}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
